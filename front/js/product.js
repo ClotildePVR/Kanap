0cc690cbc89c0cf160370949264b0f8e709d461f
Clotilde Pouvreau <pclotilde44@hotmail.fr>
2022-08-08 11:35:37 +0200

    Ajout produits dans la panier via local storage

--- front/js/product.js
+++ front/js/product.js
@@ -6,28 +6,25 @@ const host = "http://localhost:3000/api/products/";
 const productUrl = host + id
 
 // Récupérer le produit et ses détails depuis l'API
-showDetails()
+fetch(productUrl)
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(product) {
+        selectedProduct(product);
+        registredProduct(product);
+    })
+    .catch (function(error) {
+        alert(error)
+    });
 
-async function showDetails() {
-    const product = await getProducts()
-    displayProduct(product)
-}
 
-function getProducts() {
-    return fetch(productUrl)
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(product) {
-            return product;
-        })
-        .catch (function(error) {
-            alert(error)
-        });
-}
+const colorSelection = document.getElementById("colors");
+const productQuantity = document.getElementById("quantity");
+const addToCartBtn = document.getElementById("addToCart");
 
-// Insérer le produit et ses détails dans la page
-function displayProduct(product) {
+// Insérer le produit souahité et ses détails dans la page
+function selectedProduct(product) {
     
     // Image
     const productImg = document.createElement("img");
@@ -36,24 +33,87 @@ function displayProduct(product) {
     productImg.setAttribute("src", product.imageUrl);
     productImg.setAttribute("alt", product.altTxt);
     
-    //Titre
+    // Titre
     const productName = document.getElementById("title");
     productName.innerHTML = product.name;
 
-    //Prix
+    // Prix
     const productPrice = document.getElementById("price");
-    productPrice.innerHTML = product.price;    
+    productPrice.innerHTML = product.price; 
 
-    //Description
+    // Description
     const productDescription = document.getElementById("description");
     productDescription.innerHTML = product.description;
     
-    //Options de couleurs
+    // Options de couleurs
     for (let colors of product.colors) {
         const productColor = document.createElement("option");
-        let selectOption = document.getElementById("colors");
-        selectOption.appendChild(productColor);
+        colorSelection.appendChild(productColor);
         productColor.setAttribute("value", colors);
         productColor.innerHTML = colors;
     }
 }
+
+// Ajouter le produit au panier
+function registredProduct (product) {
+    
+    // Ecouter l'évènement au click du bouton "ajouter au panier"
+    addToCartBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        if (colorSelection.value == false) {
+            confirm("Veuillez sélectionner une couleur");
+        } else if (productQuantity.value == 0) {
+            confirm("Veuillez sélectionner le nombre d'articles souhaité");
+        } else {
+            alert("Votre article a bien été ajouté au panier");
+        
+        if (productQuantity.value >= 1 && productQuantity.value <= 100) {
+            
+            // Récupérer les informations du produit à ajouter au panier
+            let colorChoice = colorSelection.value;
+            let quantityChoice = productQuantity.value;
+            let productInformations = {
+                productId: id,
+                productColor: colorChoice,
+                productQuantity: quantityChoice,
+            };
+            
+            // Initialiser le local storage
+            let productLocalStorage = JSON.parse(localStorage.getItem("product"));
+
+        // Envoyer les informations du produit dans le local storage
+        if (productLocalStorage) {
+            
+        // Si le panier contient déjà un produit
+        const resultFound = productLocalStorage.find(
+            (element) => element.productId === id && element.productColor === colorChoice);
+        
+        // Si le produit dans le panier est le même que celui à ajouter : MAJ de la quantité
+            if (resultFound) {
+            let newQuantity =
+            parseInt(productInformations.productQuantity) + parseInt(resultFound.productQuantity);
+            resultFound.productQuantity = newQuantity;
+            localStorage.setItem("product", JSON.stringify(productLocalStorage));
+            console.table(productLocalStorage);
+            }
+        
+        // Si le produit dans le panier est une autre référence : ajout du produit demandé au panier
+            else {
+            productLocalStorage.push(productInformations);
+            localStorage.setItem("product", JSON.stringify(productLocalStorage));
+            console.table(productLocalStorage);
+            }
+        }
+
+        // Si le panier est vide : initier le panier
+        else {
+            productLocalStorage = [];
+            productLocalStorage.push(productInformations);
+            localStorage.setItem("product", JSON.stringify(productLocalStorage));
+            console.table(productLocalStorage);
+        }}
+        }
+    });
+}
+
